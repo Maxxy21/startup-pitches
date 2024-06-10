63843867dd9ae18c9d8d0cbd5eb125bc0c074442
Maxxy21 <nanabaah12@gmail.com>
2024-06-10 23:35:58 +0200

    Working part 1

---
+++ app/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import fs from "fs/promises";
+import {createReadStream} from "fs";
+import OpenAI from "openai";
+
+let openai: OpenAI | null = null;
+
+function getOpenAI() {
+    if (openai === null) {
+        openai = new OpenAI();
+    }
+    return openai;
+}
+
+export async function transcribeAudio(prevState: any, formData: FormData) {
+    const audioFile = formData.get("audio") as File;
+
+    const buffer = await audioFile.arrayBuffer();
+    const audioBuffer = Buffer.from(buffer);
+
+    await fs.writeFile(audioFile.name, audioBuffer);
+
+    const transcription = await getOpenAI().audio.transcriptions.create({
+        file: createReadStream(audioFile.name),
+        model: "whisper-1",
+    });
+    console.log("result", transcription.text);
+    return {result: transcription.text};
+}
