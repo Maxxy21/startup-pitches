bfc841733a1bd776d89a44cdb3a57309982ced25
Maxwell Aboagye <nanabaah12@gmail.com>
2024-12-15 15:19:16 +0100

    fixed update

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -22,7 +22,7 @@ export const RenameModal = () => {
   const { 
     mutate, 
     pending
-  } = useApiMutation(api.pitches.updatePitch);
+  } = useApiMutation(api.pitches.update);
 
   const {
     isOpen,
